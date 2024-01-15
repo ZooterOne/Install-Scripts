1c7ba230d853814919a405a8ec34b047ecd369c3
ZooterOne <ZooterOne@github.com>
2024-01-15 17:37:01 +1000

    Add VictorMono to Fonts

--- Fedora/02 - install-fonts.sh
+++ Fedora/02 - install-fonts.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 echo -e "\033[0;36m-- Installing fonts --\e[0m"
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/DaddyTimeMono.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/DaddyTimeMono.zip
 sudo unzip DaddyTimeMono.zip -d /usr/share/fonts/DaddyTimeMono
 rm DaddyTimeMono.zip
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Meslo.zip
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/Meslo.zip
 sudo unzip Meslo.zip -d /usr/share/fonts/Meslo
 rm Meslo.zip
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/VictorMono.zip
+sudo unzip VictorMono.zip -d /usr/share/fonts/VictorMono
+rm VictorMono.zip
 
 echo -e "\033[0;36m-- Updating font cache --\e[0m"
 sudo fc-cache -fv
