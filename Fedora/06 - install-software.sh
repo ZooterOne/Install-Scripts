daca5e0ea93dbabb3aed8966a6c18270440a9479
ZooterOne <ZooterOne@github.com>
2024-11-16 16:43:29 +1000

    Update development environment.

--- Fedora/06 - install-software.sh
+++ Fedora/06 - install-software.sh
@@ -23,15 +23,3 @@ sudo dnf install alacritty -y
 mkdir ~/.config/alacritty
 cp ../Alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
 endCommandGroup "Install Alacritty"
-
-startCommandGroup "Install GitHub tool"
-sudo dnf install gh -y
-endCommandGroup "Install GitHub tool"
-
-startCommandGroup "Install VSCode"
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-printf "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee -a /etc/yum.repos.d/vscode.repo
-sudo dnf install code -y
-mkdir -p ~/.config/Code/User/
-cp ../Code/settings.json ~/.config/Code/User/settings.json
-endCommandGroup "Install VSCode"
--- Fedora/08 - install-dev.sh
+++ Fedora/08 - install-dev.sh
@@ -9,3 +9,15 @@ endCommandGroup "Install .Net SDK"
 startCommandGroup "Install C++ environment"
 sudo dnf install gcc gcc-c++ gdb cmake clang autoconf automake
 endCommandGroup "Install C++ environment"
+
+startCommandGroup "Install GitHub tool"
+sudo dnf install gh -y
+endCommandGroup "Install GitHub tool"
+
+startCommandGroup "Install VSCode"
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+printf "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee -a /etc/yum.repos.d/vscode.repo
+sudo dnf install code -y
+mkdir -p ~/.config/Code/User/
+cp ../Code/settings.json ~/.config/Code/User/settings.json
+endCommandGroup "Install VSCode"
